a47182d11877d98a29ff48486fffc8e9130b776d
SkriPoxx <bastian10042000@gmail.com>
2025-08-27 09:06:25 -0600

    Add files via upload

---
+++ prime_factor_count/target/debug/deps/prime_factor_count-e4402ceacea269f4.d
@@ -0,0 +1,5 @@
+C:\Users\basti\Desktop\Rust Projects\prime_factor_count\target\debug\deps\prime_factor_count-e4402ceacea269f4.d: src\main.rs
+
+C:\Users\basti\Desktop\Rust Projects\prime_factor_count\target\debug\deps\prime_factor_count-e4402ceacea269f4.exe: src\main.rs
+
+src\main.rs:
---
+++ prime_factor_count/target/debug/prime_factor_count.d
@@ -0,0 +1,1 @@
+C:\Users\basti\Desktop\Rust\ Projects\prime_factor_count\target\debug\prime_factor_count.exe: C:\Users\basti\Desktop\Rust\ Projects\prime_factor_count\src\main.rs
