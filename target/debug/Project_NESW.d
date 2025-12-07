4530ba94512bcc7ec0c4b16e9a8c8666621f4309
SkriPoxx <bastian10042000@gmail.com>
2025-12-07 02:20:00 -0700

    Delete target directory

--- target/debug/Project_NESW.d
+++
@@ -1,1 +0,0 @@
-C:\Users\basti\Desktop\Project_NESW\target\debug\Project_NESW.exe: C:\Users\basti\Desktop\Project_NESW\src\main.rs
--- target/debug/deps/Project_NESW-2834b6159ffc59a0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\basti\Desktop\Project_NESW\target\debug\deps\Project_NESW-2834b6159ffc59a0.d: src\main.rs
-
-C:\Users\basti\Desktop\Project_NESW\target\debug\deps\Project_NESW-2834b6159ffc59a0.exe: src\main.rs
-
-src\main.rs:
